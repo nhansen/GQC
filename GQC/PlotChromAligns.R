1cdcf36564ce33b92001b6053d3d7f6f71d2c975
Nancy Hansen <nhansen@mail.nih.gov>
2025-05-05 15:29:52 -0400

    Adding plot functionality for assembly comparison

--- GQC/PlotChromAligns.R
+++ GQC/PlotChromAligns.R
@@ -1,19 +1,21 @@
 library(stringr)
 
-#setwd("/Users/nhansen/HG002_diploid_benchmark/plots/align_plots")
+#setwd("/Users/nhansen/OneDrive/HPRC_assembly_comparison/data_for_Rplots/alignplots")
 args = commandArgs(trailingOnly=TRUE)
 
-#pardefault <- par()
+pardefault <- par()
 
 chromfile <- ifelse(!( is.na(args[1])), args[1], "clustered_aligns.chr1_MATERNAL.clusters.bed")
 genomename <- ifelse(!( is.na(args[2])), args[2], "year1pat")
 benchname <- ifelse(!( is.na(args[3])), args[3], "v1.0.1")
 outputdir <- ifelse(!( is.na(args[4])), args[4], ".")
-resourcedir <- ifelse(! ( is.na(args[5])), args[5], ".")
-chromlength <- ifelse(! ( is.na(args[6])), as.integer(args[6]), NA)
+chromlength <- ifelse(! ( is.na(args[5])), as.integer(args[5]), NA)
+
+safe_colorblind_palette <- c("#332288", "#88CCEE", "#CC6677", "#DDCC77", "#117733", "#AA4499", 
+                             "#44AA99", "#999933", "#882255", "#661100", "#6699CC", "#888888")
 
 readaligns <- function(chromfile) {
-  aligns <- read.table(chromfile, sep="\t", header=FALSE)
+  aligns <- read.table(chromfile, sep="\t", header=FALSE, comment.char="!")
   names(aligns) <- c("chrom", "start", "end", "alignname")
 
   aligns$start <- as.numeric(aligns$start)/1000000
@@ -81,11 +83,13 @@ plotaligns <- function(aligns, index=1, suppressaxis=FALSE, suppresstitle=FALSE,
   contigclusters <- unique(bigcontigaligns$cluster)
   nonsmallcontigclusters <- contigclusters[!str_detect(contigclusters, "Small")]
   
-  clusterpalette <- rainbow(length(nonsmallcontigclusters))
-  if (length(clusterpalette) == 2) {
-    clusterpalette <- c("red", "blue")
-  }
+  #clusterpalette <- rainbow(length(nonsmallcontigclusters))
+  #if (length(clusterpalette) == 2) {
+    #clusterpalette <- c("red", "blue")
+  #}
 
+  clusterpalette <- safe_colorblind_palette[1:length(nonsmallcontigclusters)]
+  
   alignclustercolors <- sapply(bigcontigaligns$cluster, function(x) {
     ifelse(str_detect(x, "Small"), "black", clusterpalette[which(nonsmallcontigclusters==x, arr.ind=TRUE)] )
     })
@@ -94,7 +98,7 @@ plotaligns <- function(aligns, index=1, suppressaxis=FALSE, suppresstitle=FALSE,
     ifelse(str_detect(x, "Small"), "black", clusterpalette[which(nonsmallcontigclusters==x, arr.ind=TRUE)] )
     })
   
-  plottitle <- ifelse(suppresstitle, "", paste("Alignments of ", genomename, " to benchmark ", benchname))
+  plottitle <- ifelse(suppresstitle, "", paste("Alignments of ", genomename, " to ", benchname))
   xaxtval <- ifelse(suppressxunits, "n", "s")
   xlabval <- ifelse(suppressxunits, "", chrom)
   plot(list(), list(), main=plottitle, xaxt=xaxtval, xlab=xlabval, ylab=orderedctginfo[index, "contigname"], 
@@ -104,47 +108,54 @@ plotaligns <- function(aligns, index=1, suppressaxis=FALSE, suppresstitle=FALSE,
 }
 
 
-multiplotaligns <- function(aligns, chromlength=NA, chromplotfile=NA) {
+multiplotaligns <- function(aligns, chromlength=NA, chromplotfile=NA, plotheights=c(), minfrac=0.5) {
+  # query dataframe has only the large clusters of alignments:
   querydf <- data.frame(query=unique(aligns[!str_detect(aligns$cluster, "Small"), "query"]))
-  querydf$basescovered <-sapply(querydf$query, function(x) {querycoverage(aligns, x)})
+  querydf$querybasescovered <-sapply(querydf$query, function(x) {querycoverage(aligns, x)})
 
-  coveredbases <- sum(querydf$basescovered)
+  # don't bother to plot alignments when aligned query bases are less than minfrac (0.5) of the ref chrom length:
+  coveredbases <- sum(querydf$querybasescovered)
   halfchromlength <- 0.5*chromlength/1000000
 
   if (coveredbases < halfchromlength) {
     return(0)
   }
 
+  # open a file if there is one:
   if (!is.na(chromplotfile)) {
     pdf(chromplotfile, 8.5, 11.0)
   }
 
-  #par(pardefault)
+  par(pardefault)
   numplots <- min(length(querydf$query), 4)
 
   if (numplots==1) {
     par(mfrow = c(1, 1))
-    plotaligns(aligns, 1)
+    plotaligns(aligns, 1, chrommin=0, chrommax=floor(chromlength/1000000+1))
   }
   else {
-    orderedquerydf <- querydf[order(querydf$basescovered, decreasing=TRUE), ]
-    largestsize <- orderedquerydf[1, "basescovered"]
-    smallestsize <- orderedquerydf[length(orderedquerydf$basescovered), "basescovered"]
+    orderedquerydf <- querydf[order(querydf$querybasescovered, decreasing=TRUE), ]
+    largestsize <- orderedquerydf[1, "querybasescovered"]
+    smallestsize <- orderedquerydf[length(orderedquerydf$querybasescovered), "querybasescovered"]
     
-    sizefactor <- largestsize/4.0
-    
-    plotheights <- ifelse(as.integer(orderedquerydf$basescovered/sizefactor)>=1, as.integer(orderedquerydf$basescovered/sizefactor), 1)
-    
+    sizefactor <- largestsize/50.0
+
+    if (length(plotheights)==0) {
+      plotheights <- ifelse(as.integer(orderedquerydf$querybasescovered/sizefactor)>=1, as.integer(orderedquerydf$querybasescovered/sizefactor), 1)
+      plotheights[length(plotheights)] <- plotheights[length(plotheights)] + 15
+      #return(plotheights)
+    }
+
     par(mfrow=c(numplots, 1))
     par(oma=c(4,1,3,1))
-    nf <- layout(matrix(seq(1, numplots),ncol=1), widths=rep(5, numplots), heights=plotheights, TRUE)
+    nf <- layout(matrix(seq(1, numplots),ncol=1), widths=rep(80, numplots), heights=plotheights, TRUE)
     
     par(mar=c(0,4,2,1))
-    plotaligns(aligns, 1, suppressaxis=TRUE)
+    plotaligns(aligns, 1, suppressaxis=TRUE, chrommin=0, chrommax=floor(chromlength/1000000+1), suppressxunits=TRUE)
     if (numplots >= 3) {
        for (plotno in seq(2, numplots-1)) {
          par(mar=c(0,4,0,1))
-         plotaligns(aligns, plotno, suppressaxis=TRUE, suppresstitle=TRUE)
+         plotaligns(aligns, plotno, suppressaxis=TRUE, suppressxunits=TRUE, suppresstitle=TRUE, chrommin=0, chrommax=floor(chromlength/1000000+1))
        }
     }
     par(mar=c(5,4,0,1))
